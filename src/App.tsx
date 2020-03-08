be00d4ba76cde172d7ac59d47e927a483d37ee17
Koban Davis <kobandavis@gmail.com>
2020-03-08 18:09:51 +0000

    Remove old code

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,7 @@
 import React, { FC, useEffect } from 'react'
 
-import Layout from './components/Layout'
-import { useGame } from './providers/GameProvider'
-import { useUser } from './providers/UserProvider'
-import InitGame from './components/InitGame'
-import End from './components/End'
-
 const App: FC = () => {
-	const { gameState } = useGame()
-	const { user, setPosition } = useUser()
-	useEffect(() => {
-		if (gameState.isGameLoaded) {
-			if (user.position === null) setPosition(gameState.start)
-		}
-	}, [gameState.isGameLoaded])
-
-	return gameState.playerWon !== null ? <End /> : gameState.isGameLoaded ? <Layout /> : <InitGame />
+	return <div>Template</div>
 }
 
 export default App
--- src/index.tsx
+++ src/index.tsx
@@ -3,18 +3,5 @@ import ReactDOM from 'react-dom'
 import App from './App'
 
 import './index.less'
-import { GameProvider } from './providers/GameProvider'
-import { UserProvider } from './providers/UserProvider'
-import { DialogueProvider } from './providers/DialogueProvider'
 
-const AppWithProviders: FC = () => (
-	<DialogueProvider>
-		<GameProvider>
-			<UserProvider>
-				<App />
-			</UserProvider>
-		</GameProvider>
-	</DialogueProvider>
-)
-
-ReactDOM.render(<AppWithProviders />, document.getElementById('app'))
+ReactDOM.render(<App />, document.getElementById('app'))
